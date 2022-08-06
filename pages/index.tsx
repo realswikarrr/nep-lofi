d644ff40047e937ac7c12ce2527020c3b145be2e
realswikarrr <swikara082@gmail.com>
2022-08-06 10:58:23 +0545

    feat: 🔮 Tailwind Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello Lofi Nep</h1>
+      <h1 className="text-4xl">Hello Lofi Nep</h1>
     </div>
   );
 };
