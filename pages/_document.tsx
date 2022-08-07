84615d2e3aa36128f59b1e450e195edac473789b
realswikarrr <swikara082@gmail.com>
2022-08-07 20:52:11 +0545

    fix: 🐉 Fixed Some Style Bugs

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-my-bg w-full h-screen bg-no-repeat bg-center bg-cover">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
