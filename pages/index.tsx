6c2365dcc5220c745f7218e82e86f64f39acc571
realswikarrr <swikara082@gmail.com>
2022-08-07 13:53:14 +0545

    feat: 🔮 Added Background And Box

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      <h1>Hello</h1>
-    </div>
+    <div className="mainbg h-[90%] w-[90%] rounded-xl overflow-hidden shadow-2xl bg-cover bg-center border-2 border-[#674AB3] shadow-5xl"></div>
   );
 };
 
