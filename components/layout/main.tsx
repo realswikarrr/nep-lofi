35d80e5c21b5517d4e2d2e61644ce7c90a96db27
realswikarrr <swikara082@gmail.com>
2022-08-08 10:03:21 +0545

    feat: 🔮 Changed Favicon Icon

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -14,16 +14,7 @@ const Main = ({ children, id, playing }: Props) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Nep Lofi</title>
-        <meta name="description" content="Nep Lofi HomePage" />
-        <meta name="author" content="Swikar Adhikari" />
-        <meta name="author" content="swikarr_" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta property="og:site_name" content="Swikar Adhikari" />
-        <meta name="og:title" content="Swikar Adhikari" />
-        <meta property="og:type" content="website" />
       </Head>
 
       <Player id={id} playing={playing} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Nep Lofi HomePage" />
+        <meta name="author" content="Swikar Adhikari" />
+        <meta name="author" content="swikarr_" />
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.ico"
+          type="image/x-icon"
+        />
+        <meta property="og:site_name" content="Swikar Adhikari" />
+        <meta name="og:title" content="Swikar Adhikari" />
+        <meta property="og:type" content="website" />
+      </Head>
+
       <body className="bg-my-bg w-full h-screen bg-no-repeat bg-center bg-cover">
         <Main />
         <NextScript />
