20c9fc9b2caf7eb0e4a7edafbcf075b129606843
realswikarrr <swikara082@gmail.com>
2022-08-10 21:41:40 +0545

    fix: 🐉 Fixed Some Sign In Issues

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,11 +1,11 @@
 import Layout from "../components/layout/article";
 import SignInLayout from "../components/signin";
-import { useSession, signOut } from "next-auth/react";
+import { useSession, signOut, getSession } from "next-auth/react";
 
 const Chat = () => {
-  const { data: session } = useSession();
+  const { data: session, status }: any = useSession();
 
-  if (!session) {
+  if (status === "unauthenticated") {
     return (
       <Layout>
         <SignInLayout />
@@ -14,7 +14,7 @@ const Chat = () => {
   } else {
     return (
       <Layout>
-        <h1 className="text-white">Welcome {session.user?.name}</h1>
+        <h1 className="text-white">Welcome {session.user.name}</h1>
         <button
           onClick={() => signOut()}
           className="flex items-center border-2 border-[#674AB3] px-8 py-3 text-sm font-medium text-white transition rounded hover:scale-110 hover:shadow-xl active:bg-[#674AB3] focus:outline-none "
@@ -27,3 +27,12 @@ const Chat = () => {
 };
 
 export default Chat;
+
+export const getServerSideProps = async (ctx: any) => {
+  const session = await getSession(ctx.req);
+  return {
+    props: {
+      session,
+    },
+  };
+};
