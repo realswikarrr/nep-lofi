00c3daad68614cbb96826ac9a64fbe0d3ed228f7
realswikarrr <swikara082@gmail.com>
2022-08-12 17:11:32 +0545

    feat: 🔮 Added Loading State

--- components/chats.tsx
+++ components/chats.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { supabase } from "../utils/supabaseClient";
+import { useEffect } from "react";
 import Layout from "./layout/article";
-import LoadingSvg from "./loading-svg";
 import { useRef } from "react";
 
-const Chats = () => {
+const Chats = ({ chats }: any) => {
   const messagesEndRef = useRef<null | HTMLDivElement>(null);
-  const [chats, setChats] = useState<any>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -18,65 +14,37 @@ const Chats = () => {
     scrollToBottom();
   }, [chats]);
 
-  useEffect(() => {
-    const getChats = async () => {
-      const { data } = await supabase.from("chats").select();
-      setChats(data);
-    };
-    getChats();
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    const subscription = supabase
-      .from("chats")
-      .on("INSERT", (payload) => {
-        setChats((current: any) => [...current, payload.new]);
-      })
-      .subscribe();
-
-    return () => {
-      supabase.removeSubscription(subscription);
-    };
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <LoadingSvg />
-      ) : (
-        <Layout delay="0.7">
-          <div className="flex flex-col flex-grow p-4  overflow-y-scroll h-screen max-w-xl mx-auto w-full mb-2 mt-2">
-            {chats?.map((chat: any) => (
-              <div
-                key={chat.id}
-                className="flex items-center gap-2 border-2 border-[#674AB3]  rounded-lg p-2 mb-2"
-              >
-                <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-                  <Image
-                    src={chat.image_url}
-                    alt="user picture"
-                    width={50}
-                    height={50}
-                    className="rounded-lg"
-                  />
-                </div>
-                <div>
-                  <h1 className="text-white">{chat.content}</h1>
-                  <span className="text-xs text-gray-300 leading-none mr-2">
-                    {chat.name}
-                  </span>
-                  <span className="text-xs text-gray-300 leading-none">
-                    {chat.created_at}
-                  </span>
-                </div>
-              </div>
-            ))}
-            <div ref={messagesEndRef} />
+    <Layout delay="0.7">
+      <div className="flex flex-col flex-grow p-4  overflow-y-scroll h-screen max-w-xl mx-auto w-full mb-2 mt-2">
+        {chats?.map((chat: any) => (
+          <div
+            key={chat.id}
+            className="flex items-center gap-2 border-2 border-[#674AB3]  rounded-lg p-2 mb-2"
+          >
+            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+              <Image
+                src={chat.image_url}
+                alt="user picture"
+                width={50}
+                height={50}
+                className="rounded-lg"
+              />
+            </div>
+            <div>
+              <h1 className="text-white">{chat.content}</h1>
+              <span className="text-xs text-gray-300 leading-none mr-2">
+                {chat.name}
+              </span>
+              <span className="text-xs text-gray-300 leading-none">
+                {chat.created_at}
+              </span>
+            </div>
           </div>
-        </Layout>
-      )}
-    </>
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+    </Layout>
   );
 };
 
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,14 +1,48 @@
 import Chats from "../components/chats";
 import Layout from "../components/layout/article";
 import SignInLayout from "../components/signin";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 import FormLayout from "../components/form";
+import LoadingSvg from "../components/loading-svg";
 
 const Chat = () => {
+  const [chats, setChats] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getChats = async () => {
+      const { data } = await supabase.from("chats").select();
+      setChats(data);
+      setLoading(false);
+    };
+    getChats();
+  }, []);
+
+  useEffect(() => {
+    const subscription = supabase
+      .from("chats")
+      .on("INSERT", (payload) => {
+        setChats((current: any) => [...current, payload.new]);
+      })
+      .subscribe();
+
+    return () => {
+      supabase.removeSubscription(subscription);
+    };
+  }, []);
+
   return (
     <Layout delay="0.7">
       <div className="flex container mx-auto flex-col flex-grow w-full max-w-xl shadow-xl rounded-lg overflow-hidden">
-        <Chats />
+        {loading ? (
+          <div className="h-screen flex justify-center">
+            <LoadingSvg text="Loading Chats.." />{" "}
+          </div>
+        ) : (
+          <Chats chats={chats} />
+        )}
         <FormLayout />
       </div>
       <SignInLayout />
