4a0d14a1062578c5698929d38bdbf0d1570a29fd
Swikar Adhikari <swikara082@gmail.com>
2022-08-14 17:02:51 +0545

    fix: 🐛 Alarm Not Working On Iphone

--- components/alarm.tsx
+++ components/alarm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Alarm = React.forwardRef((_, ref: any) => {
   return (
-    <audio ref={ref}>
+    <audio ref={ref} preload="auto">
       <source src="/alarm.mp3" type="audio/mpeg" />
       Your browser does not support the audio element.
     </audio>
